c9e0052538c77537412f1cfbbc55b44629761cde
ivaniuss <ivan.emi94@gmail.com>
2024-05-14 22:38:40 -0500

    auth done

---
+++ src/app.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { Public } from './auth/decorators/public.decorator';
+
+@Controller()
+export class AppController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @Public()
+  @Get('findall')
+  findAll() {
+    return [];
+  }
+}
